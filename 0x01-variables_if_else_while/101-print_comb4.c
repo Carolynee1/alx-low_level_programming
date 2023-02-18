2f628b8c55aae4a38cf54a12e3651f5dc64de94a
Carolynee1 <c.ajisegiri9@gmail.com>
2023-02-17 17:00:18 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,6 +12,7 @@ int main(void)
 {
 
 	int x, y, z;
+
 	for (x = '0'; x < '9'; x++)
 	{
 
@@ -20,7 +21,7 @@ int main(void)
 
 	for (z = y + 1; z <= '9'; z++)
 	{
-	if((y != x) != z)
+	if ((y != x) != z)
 	{
 	putchar(x);
 	putchar(y);
