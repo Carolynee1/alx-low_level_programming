9f9e8d9da7f082f1385641bfb66f27c701a2542a
Carolynee1 <c.ajisegiri9@gmail.com>
2023-02-22 18:04:25 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
 * _isalpha - Checks for alphabetic character
 * @c: The character to be checked
-* Return: 1 for alphabet character or 0 for anything
+* Return: 1 for alphabet character or 0 for anything else
 */
 
 int _isalpa(int c)
