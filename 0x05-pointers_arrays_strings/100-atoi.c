51fe7208d6aa21bed13547ced3e96fdc33e8b174
Carolynee1 <c.ajisegiri9@gmail.com>
2023-02-27 17:17:56 -0800

    task  10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,13 +12,13 @@ int _atoi(char *s)
 	int c = 0;
 	unsigned int ni = 0;
 	int min = 1;
-	ini isi = 0;
+	int isi = 0;
 
 	while (s[c])
 	{
 	if (s[c] == 45)
 	{
-	min == -1;
+	min *= -1;
 	}
 	while (s[c] >= 48 && s[c] <= 57)
 	{
