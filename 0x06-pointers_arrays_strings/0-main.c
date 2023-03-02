51e9f9856912def5c636826cbe5c971437beab76
Carolynee1 <c.ajisegiri9@gmail.com>
2023-03-02 09:11:39 -0800

    0

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!\n";
 	char *ptr;
-	
+
 	printf("%s\n", s1);
 	printf("%s", s2);
 	ptr = _strcat(s1, s2);
