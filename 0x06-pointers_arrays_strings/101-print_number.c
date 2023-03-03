5c1082a71ed03c34e77dcec8fa36015909bd1ae3
Carolynee1 <c.ajisegiri9@gmail.com>
2023-03-03 10:27:04 -0800

    t9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_h
+#include "main.h"
 
-#include <stdio.h>
 
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-#endif
+/**
+ * print_number - Print an integer.
+ * @n: The integer to be printed.
+ *
+ */
+
+void print_number(int n)
+{
+
+	unsigned int num =n;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	num = -num;
+	}
+
+	if ((num / 10) > 0)
+	print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
