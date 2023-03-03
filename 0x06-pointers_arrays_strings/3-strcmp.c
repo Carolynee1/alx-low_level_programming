b405d618f61f386ee6016bb9551eea7f8d43d741
Carolynee1 <c.ajisegiri9@gmail.com>
2023-03-03 01:35:29 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcmp -function that Compares two strings.
- * @s1: first string to be compared.
- * @s2: second string to be compared.
+ * @s1: first string
+ * @s2: second string
  *
  * Return: value of string
  */
@@ -11,11 +11,12 @@
 int _strcmp(char *s1, char *s2)
 
 {
+	
 	int i = 0;
 
 	while (s1[i] - s2[i] == 0 && s1[i]! = '\0')
 	{
-	i++;
+		i++;
 	}
 	return  (s1[i] - s2[i]);
 
