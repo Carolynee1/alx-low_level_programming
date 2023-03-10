e2378f7a561fe3d8fe44af99b0329ade52a95307
Carolynee1 <c.ajisegiri9@gmail.com>
2023-03-10 14:04:58 -0800

    t2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,11 +3,12 @@
 
 /**
  *main - Prints all the args content
- @argc: argument count
- @argv: argument vector
+ *@argc: argument count
+ *@argv: argument vector
  *
  * Return: Always zero
  */
+
 int main(int argc, char *argv[])
 {
 	int i;
