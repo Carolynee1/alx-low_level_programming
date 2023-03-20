723f254c492e291ae7b62104d023ee368974db3b
Carolynee1 <c.ajisegiri9@gmail.com>
2023-03-20 13:33:37 -0700

    t2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the file program was compiled from
+ * main - Prints the name of the file program was compiled from.
  * Return: Always 0.
  */
 int main(void)
 {
-
 	printf("%s\n", "2-main.c");
 	return (0);
 }
