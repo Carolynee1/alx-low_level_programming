50abbfceff79c616809b40baba6f5bb0c8776c61
Carolynee1 <c.ajisegiri9@gmail.com>
2023-03-22 16:33:13 -0700

    t3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,7 +47,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if ( b == 0)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -70,5 +70,5 @@ int op_mod(int a, int b)
 	printf("Erroor\n");
 	exit(100);
 }
-return ( a % b);
+return (a % b);
 }
