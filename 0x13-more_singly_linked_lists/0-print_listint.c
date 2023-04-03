501590390ff76590d84774ab0d0f03e4d4403db3
Carolynee1 <c.ajisegiri9@gmail.com>
2023-04-03 15:32:27 -0700

    t0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t alp = 0;
+size_t num = 0;
 
 while (h)
 {
 printf("%d\n", h->n);
-alp++;
+num++;
 h = h->next;
 }
 
-return (alp);
+return (num);
 }
 
