b008bb444e4c6f5d18abd909314a667fffb18360
Carolynee1 <c.ajisegiri9@gmail.com>
2023-04-04 02:34:07 -0700

    t14

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
+ *
+ * Return: The address of the node where the loop starts, or NULL 
+ * if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *weak = head;
+listint_t *tough = head;
+
+if (!head)
+return (NULL);
+
+while (weak && tough && tough->next)
+{
+tough = tough->next->next;
+weak = weak->next;
+if (tough == weak)
+{
+weak = head;
+while (slow != tough)
+{
+weak = weak->next;
+tough = tough->next;
+}
+return (tough);
+}
+}
+
+return (NULL);
+}
