d2376487a2344e7f0ec7a13095be1a67d90658b2
Carolynee1 <c.ajisegiri9@gmail.com>
2023-05-08 04:42:28 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,18 +11,21 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
-
+       	int fd, bytes_written;
+	{
 
 		if (filename == NULL)
 			return (-1);
+	}
 
 	if (access(filename, W_OK) == -1)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
+	{
 		return (-1);
+	}
 
 	if (text_content == NULL)
 	{
