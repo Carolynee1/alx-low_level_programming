d10436c2544f1b9e7812d1b1c7996b6c5e022b38
Carolynee1 <c.ajisegiri9@gmail.com>
2023-05-08 04:23:54 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
 	{
 		num_letters = strlen(text_content);
 
